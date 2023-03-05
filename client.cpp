cd69b10dc4c3d34d3d5be244b80259850a379cdc
hc6674 <hc6674@rit.edu>
2023-03-05 02:26:40 +0000

    update

--- client.cpp
+++ client.cpp
@@ -1,38 +1,88 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 #include <sys/socket.h>
+#include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
-#include <string.h>
 
-#define PORT 8080
+using namespace std;
 
-int main(int argc, char const *argv[]) {
-    int sock = 0, valread;
+const char* SERVER_IP = "127.0.0.1";
+const int PORT = 8080;
+
+int main() {
+    int sock = 0;
     struct sockaddr_in serv_addr;
-    char *hello = "Hello from client";
     char buffer[1024] = {0};
 
-    // create socket file descriptor
+    // Create a socket for the client
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        std::cout << "socket creation error" << std::endl;
-        return -1;
+        cerr << "Socket creation error" << endl;
+        return 1;
     }
 
-    // set server address and port
+    // Set server address and port
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(PORT);
-
-    // convert IP address from string to binary form
-    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0) {
-        std::cout << "invalid address" << std::endl;
-        return -1;
+    if (inet_pton(AF_INET, SERVER_IP, &serv_addr.sin_addr) <= 0) {
+        cerr << "Invalid address/ Address not supported" << endl;
+        return 1;
     }
 
-    // connect to server
+    // Connect to the server
     if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-        std::cout << "connection failed" << std::endl;
-        return -1;
+        cerr << "Connection failed" << endl;
+        return 1;
     }
 
-   
+    while (true) {
+        // Receive a message from the server
+        int valread = read(sock, buffer, 1024);
+        cout <<buffer[valread] = '\0';
+    cout << buffer;
+
+    // Get user input
+    string input;
+    getline(cin, input);
+
+    // Send user input to the server
+    send(sock, input.c_str(), input.length(), 0);
+
+    // Receive a message from the server
+    valread = read(sock, buffer, 1024);
+    buffer[valread] = '\0';
+    cout << buffer;
+
+    // If the server is sending a file, receive it
+    if (strncmp(buffer, "Sending", 7) == 0) {
+        // Extract the filename
+        string filename = buffer + 8;
+        filename = filename.substr(0, filename.find(" to client"));
+
+        // Receive the file size
+        valread = read(sock, buffer, 1024);
+        buffer[valread] = '\0';
+        int filesize = stoi(buffer);
+
+        // Receive the file data
+        ofstream outfile(filename, ios::out | ios::binary);
+        if (!outfile.is_open()) {
+            cerr << "Could not create file" << endl;
+        } else {
+            int bytes_received = 0;
+            while (bytes_received < filesize) {
+                valread = read(sock, buffer, 1024);
+                bytes_received += valread;
+                outfile.write(buffer, valread);
+            }
+            outfile.close();
+            cout << "File received" << endl;
+        }
+    }
+}
+
+close(sock);
+
+return 0;
 }
--- server.cpp
+++ server.cpp
@@ -1,65 +1,233 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
+#include <arpa/inet.h>
+#include <net/if.h>
+#include <ifaddrs.h>
+#include <netinet/if_ether.h>
+#include <sys/types.h>
+#include <pwd.h>
 
-#define PORT 8080
+using namespace std;
 
-int main(int argc, char const *argv[]) {
-    int server_fd, new_socket, valread;
-    struct sockaddr_in address;
+const int PORT = 8080;
+
+// Function to get the client's IP address
+string get_client_ip(int client_fd) {
+    struct sockaddr_in client_addr;
+    socklen_t client_addr_len = sizeof(client_addr);
+    if (getpeername(client_fd, (struct sockaddr *)&client_addr, &client_addr_len) < 0) {
+        cerr << "Getpeername error" << endl;
+        return "";
+    }
+    return inet_ntoa(client_addr.sin_addr);
+}
+
+// Function to get the client's MAC address
+string get_client_mac(int client_fd) {
+    struct sockaddr_ll client_addr;
+    socklen_t client_addr_len = sizeof(client_addr);
+    if (getpeername(client_fd, (struct sockaddr *)&client_addr, &client_addr_len) < 0) {
+        cerr << "Getpeername error" << endl;
+        return "";
+    }
+    char mac[18];
+    sprintf(mac, "%02x:%02x:%02x:%02x:%02x:%02x",
+            client_addr.sll_addr[0], client_addr.sll_addr[1], client_addr.sll_addr[2],
+            client_addr.sll_addr[3], client_addr.sll_addr[4], client_addr.sll_addr[5]);
+    return string(mac);
+}
+
+// Function to get the client's username
+string get_client_username() {
+    uid_t uid = geteuid();
+    struct passwd *pw = getpwuid(uid);
+    if (pw == NULL) {
+        cerr << "Getpwuid error" << endl;
+        return "";
+    }
+    return string(pw->pw_name);
+}
+
+// Function to get the client's operating system
+string get_client_os() {
+    int mib[2];
+    mib[0] = CTL_KERN;
+    mib[1] = KERN_OSRELEASE;
+    char os_release[256];
+    size_t len = sizeof(os_release);
+    if (sysctl(mib, 2, os_release, &len, NULL, 0) < 0) {
+        cerr << "Sysctl error" << endl;
+        return "";
+    }
+    return string(os_release);
+}
+
+// Function to list out currently running processes
+string list_processes() {
+    string output = "";
+    FILE *pipe = popen("ps -ax", "r");
+    char buffer[256];
+    while (fgets(buffer, sizeof(buffer), pipe)) {
+        output += buffer;
+    }
+    pclose(pipe);
+    return output;
+}
+
+int main() {
+    int server_fd, client_fd;
+    struct sockaddr_in server_addr, client_addr;
     int opt = 1;
-    int addrlen = sizeof(address);
+    int addrlen = sizeof(server_addr);
     char buffer[1024] = {0};
-    char *hello = "Hello from server";
+    string filename;
+    ofstream outfile;
 
-    // create socket file descriptor
+    // Create a socket for the server
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-        perror("socket failed");
-        exit(EXIT_FAILURE);
+        cerr << "Socket creation error" << endl;
+        return 1;
     }
 
-    // set socket options to allow reuse of address and port
+    // Set socket options
+   
     if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
-        perror("setsockopt");
-        exit(EXIT_FAILURE);
+        cerr << "Setsockopt error" << endl;
+        return 1;
     }
 
-    // set server address and port
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(PORT);
-
-    // bind socket to address and port
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-        perror("bind failed");
-        exit(EXIT_FAILURE);
+    // Bind the socket to a port
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(PORT);
+    if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        cerr << "Bind error" << endl;
+        return 1;
     }
 
-    // listen for incoming connections
+    // Listen for incoming connections
     if (listen(server_fd, 3) < 0) {
-        perror("listen");
-        exit(EXIT_FAILURE);
+        cerr << "Listen error" << endl;
+        return 1;
     }
 
-    // accept incoming connection
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
-        perror("accept");
-        exit(EXIT_FAILURE);
+    while (true) {
+        // Accept a client connection
+        if ((client_fd = accept(server_fd, (struct sockaddr *)&client_addr, (socklen_t*)&addrlen)) < 0) {
+            cerr << "Accept error" << endl;
+            return 1;
+        }
+
+        // Send a message to the client
+        string message = "Choose an action:\n1. List running processes\n2. Upload/download a file\n3. Send client information\n";
+        send(client_fd, message.c_str(), message.length(), 0);
+
+        // Receive the client's choice
+        int valread = read(client_fd, buffer, 1024);
+        char choice = buffer[0];
+
+        // List out currently running processes
+        if (choice == '1') {
+            message = list_processes();
+            send(client_fd, message.c_str(), message.length(), 0);
+        }
+        // Upload or download a file
+        else if (choice == '2') {
+            // Prompt the client for the action
+            message = "Choose an action:\n1. Upload a file\n2. Download a file\n";
+            send(client_fd, message.c_str(), message.length(), 0);
+
+            // Receive the client's choice
+            valread = read(client_fd, buffer, 1024);
+            char file_choice = buffer[0];
+
+            // Prompt the client for the filename
+            message = "Enter filename: ";
+            send(client_fd, message.c_str(), message.length(), 0);
+
+            // Receive the filename from the client
+            valread = read(client_fd, buffer, 1024);
+            buffer[valread] = '\0';
+            filename = buffer;
+
+            // Upload a file
+            if (file_choice == '1') {
+                message = "Receiving " + filename + " from client";
+                send(client_fd, message.c_str(), message.length(), 0);
+                outfile.open(filename, ios::out | ios::binary);
+                if (!outfile.is_open()) {
+                    message = "Could not create file";
+                    send(client_fd, message.c_str(), message.length(), 0);
+                } else {
+                    valread = read(client_fd, buffer, 1024);
+                    int filesize = stoi(buffer);
+                    message = "File size: " + to_string(filesize) + " bytes";
+                    send(client_fd, message.c_str(), message.length(), 0);
+                    int bytes_received = 0;
+                    while (bytes_received < filesize) {
+                        valread = read(client_fd, buffer, 1024);
+                        bytes_received += valread;
+                        outfile.write(buffer, valread);
+                    }
+                    message = "File received";
+                    send(client_fd, message.c_str(), message.length(), 0);
+                    outfile.close();
+                }
+            }
+            // Download a file
+            else if (file_choice == '2') {
+                ifstream infile(filename);
+                if (!infile.good()) {
+                
+                    message = "File not found";
+                    send(client_fd, message.c_str(), message.length(), 0);
+                } else {
+                    message = "Sending " + filename + " to client";
+                    send(client_fd, message.c_str(), message.length(), 0);
+                    infile.seekg(0, ios::end);
+                    int filesize = infile.tellg();
+                    infile.seekg(0, ios::beg);
+                    message = to_string(filesize);
+                    send(client_fd, message.c_str(), message.length(), 0);
+                    char filedata[1024] = {0};
+                    while (!infile.eof()) {
+                        infile.read(filedata, 1024);
+                        send(client_fd, filedata, infile.gcount(), 0);
+                    }
+                    infile.close();
+                }
+            }
+            // Invalid choice
+            else {
+                message = "Invalid choice";
+                send(client_fd, message.c_str(), message.length(), 0);
+            }
+        }
+        // Send client information to the server
+        else if (choice == '3') {
+            string ip_address = get_client_ip(client_fd);
+            string mac_address = get_client_mac(client_fd);
+            string username = get_client_username();
+            string os = get_client_os();
+            message = "IP address: " + ip_address + "\nMAC address: " + mac_address + "\nUsername: " + username + "\nOperating System: " + os;
+            send(client_fd, message.c_str(), message.length(), 0);
+        }
+        // Invalid choice
+        else {
+            message = "Invalid choice";
+            send(client_fd, message.c_str(), message.length(), 0);
+        }
+
+        // Close the client socket
+        close(client_fd);
     }
 
-    // read data from client
-    valread = read(new_socket, buffer, 1024);
-    printf("%s\n", buffer);
-
-    // send data back to client
-    send(new_socket, hello, strlen(hello), 0);
-    printf("Hello message sent\n");
-
-    // close connection
-    close(new_socket);
+    // Close the server socket
     close(server_fd);
 
     return 0;
